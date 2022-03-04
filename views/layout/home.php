c1cef39d72010efecff1d91a6cbdfe9beb514eda
TheViccs <victordelrio1411@gmail.com>
2022-03-04 00:02:58 -0600

    Venta Home

--- views/layout/home.php
+++ views/layout/home.php
@@ -17,6 +17,7 @@
       <img src="../../assets/img/itcolima.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
       SiGAC
     </a>
+    <h1 style="color: white; text-align: center;">SELECCIONE EL TIPO DE USUARIO</h1>
   </div>
 </nav>
 
@@ -26,61 +27,51 @@
 
 
   <body>
-    <h1 style="text-align: center;">SELECCIONE EL TIPO DE USUARIO</h1>
+    
     <div class="container">
       
-      <div class="btn">
+      <div class="btnAdmin">
         <a href="http://localhost/Extraescolares/views/modules/admin/administrador.php" id="boton">
-          <button type="button" class="btn btn-primary">Administrador</button>
+          <button type="button" class="btn btn-dark">Administrador</button>
         </a>
     </div>
 
-    <div class="btn">
+    <div class="btnAdmin">
       <a href="" >
-        <button type="button" class="btn btn-primary">Responsable Programas</button>
+        <button type="button" class="btn btn-dark">Responsable Programas</button>
       </a>
   </div>
+  
+  
+  <div class="btnAdmin">
+    <a href="" >
+      <button type="button" class="btn btn-dark">Instructor</button>
+    </a>
+  </div>
+
+  <div class="btnAdmin">
+    <a href="" >
+      <button type="button" class="btn btn-dark">Estudiantes</button>
+    </a>
+  </div>
+
+<div class="btnAdmin">
+  <a href="" >
+    <button type="button" class="btn btn-primary">Directivos</button>
+  </a>
+</div>
 
 
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </body>
-</html>  
-
-
-<!---
-      
-        <div>
-          <a href="http://localhost/Extraescolares/views/modules/admin/administrador.php" id="boton">
-            <button type="button" class="btn btn-primary">Administrador</button>
-          </a>
-      </div>
-
-      <div >
-          <a href="">
-            <button type="button" class="btn btn-primary">Responsable de Programas</button>
-          </a>
-      </div>
-
-      <div>
-          <a href="">
-            <button type="button" class="btn btn-primary">Directivos</button>
-          </a>
-      </div>
-
-      <div>
-          <a href="">
-            <button type="button" class="btn btn-primary">Instructores</button>
-
-          </a>
-      </div>
-      <button type="button" class="btn btn-primary">Alumnos</button>
-      <div >
-        
-      </div>
-      </div>
-    </div>
-
-    </div>    -->   
     
+  </body>
+  <div class="footer h-20 w-100 bottom-0" style="position: absolute;">
+    <nav class="navbar navbar-light bg-light h-100 d-flex justify-content-center">
+        <div class="d-flex justify-content-center h-100" >
+            <a class="navbar-brand text-center" href="#">Tecnológico Nacional de Mexico Campus Colima</a>
+        </div>
+    </nav>
+</div>
+</html>  
