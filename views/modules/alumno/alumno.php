d222f58c68f753d5509eb3471e9c924eccae2fb0
Ricardo Baeza <jricardobaeza@gmail.com>
2022-05-18 21:30:57 -0500

    Base de datos completa y modificacion input modal gestionar unidades responsables

--- views/modules/alumno/alumno.php
+++ views/modules/alumno/alumno.php
@@ -10,23 +10,31 @@
     <style>
         .main {
             width: 100% !important;
-            min-height: calc(100% - 137px) !important;
-            overflow-y: auto;
+            height: auto;
             display: inline-grid;
             grid-template-columns: 1fr;
             grid-template-rows: auto;
             grid-gap: 10px;
-            justify-content: center;
             align-items: center;
             justify-items: center;
         }
 
+        .boton-mostrar-grupos {
+            border-radius: 10px;
+        }
+
+        .boton-mostrar-grupos:hover {
+            background-color: orange;
+        }
+
         .content {
             width: 100%;
+            height: 100%;
             display: flex;
             flex-direction: column;
-            justify-content: center;
+            justify-content: flex-start;
             align-items: center;
+            overflow-y: auto;
         }
 
         .contenedor-programa {
@@ -39,7 +47,7 @@
             border-radius: 10px;
         }
 
-        .contenedor-programa-titulo{
+        .contenedor-programa-titulo {
             width: 100%;
             height: 50px;
             display: flex;
@@ -49,7 +57,7 @@
             border-radius: 10px;
         }
 
-        .titulo-programa{
+        .titulo-programa {
             padding-left: 20px;
         }
 
@@ -64,8 +72,39 @@
             flex-direction: column;
             justify-content: center;
             align-items: center;
-            border: 1px solid black;
             border-radius: 10px;
+        }
+
+        .contenedor-actividad {
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+            grid-auto-rows: auto;
+            justify-items: center;
+            align-items: center;
+        }
+
+        .contenedor-actividad-caracteristicas {
+            width: 80%;
+            padding-top: 8px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .contenedor-actividad-boton {
+            grid-column-start: 1;
+            grid-column-end: 3;
+            grid-row-start: 3;
+            grid-row-end: 4;
+            width: 80%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding-bottom: 8px;
         }
 
         .contenedor-actividades-titulo {
@@ -77,6 +116,10 @@
             border: 1px solid black;
             border-radius: 10px;
             background-color: lightgray;
+        }
+
+        .contenedor-actividades-titulo:hover {
+            cursor: pointer;
         }
 
         .contenedor-grupos {
@@ -99,7 +142,7 @@
             border-radius: 10px;
         }
 
-        .contenedor-grupo-titulo{
+        .contenedor-grupo-titulo {
             width: 100%;
             height: 50px;
             display: flex;
@@ -109,17 +152,20 @@
             border-radius: 10px;
             background-color: white;
         }
-        
-        .descripcion-grupo{
+
+        .contenedor-grupo-titulo:hover{
+            cursor: pointer;
+        }
+
+        .descripcion-grupo {
             width: 100%;
             height: 300px;
         }
-
     </style>
 </head>
 
 <body>
-    <div class="content h-100 w-100 d-flex flex-column bg-white">
+    <div class="h-100 w-100 d-flex flex-column bg-white">
         <!-- HEADER -->
         <?php include "../../../views/layout/header.php" ?>
 
@@ -128,6 +174,7 @@
             <div class="content">
                 <h1>Actividades</h1>
                 <div class="main">
+                    
 
                 </div>
             </div>
@@ -183,18 +230,34 @@
             let actividades = [{
                 id_actividad: "1",
                 nombre: "Futbol",
+                descripcion: "eferf efr ferferf erferf erferfe erfer ferferf erferferf erferfr rferf rfrfre rferferf erferf erfer erfee rferf erfe rferferf",
+                competencia: "eferf efr ferferf erferf erferfe erfer ferferf erferferf erferfr rferf rfrfre rferferf erferf erfer erfee rferf erfe rferferf",
+                creditos_otorga: 1,
+                beneficios: "eferf efr ferferf erferf erferfe erfer ferferf erferferf erferfr rferf rfrfre rferferf erferf erfer erfee rferf erfe rferferf",
                 id_programa: "1"
             }, {
                 id_actividad: "2",
                 nombre: "Basquetbol",
+                descripcion: "",
+                competencia: "",
+                creditos_otorga: 1,
+                beneficios: "",
                 id_programa: "1"
             }, {
                 id_actividad: "3",
                 nombre: "Danza",
+                descripcion: "",
+                competencia: "",
+                creditos_otorga: 1,
+                beneficios: "",
                 id_programa: "2"
             }, {
                 id_actividad: "4",
                 nombre: "Tutoria",
+                descripcion: "",
+                competencia: "",
+                creditos_otorga: 1,
+                beneficios: "",
                 id_programa: "3"
             }]
             actividades.forEach(actividad => {
@@ -207,11 +270,75 @@
                 let h3 = document.createElement("h3");
                 h3.textContent = actividad.nombre;
                 divTitulo.appendChild(h3);
+                let contenedorActividad = document.createElement("div");
+                contenedorActividad.classList.add("contenedor-actividad");
+                contenedorActividad.setAttribute("hidden", true);
+
+                let divContenedorDescripcion = document.createElement("div");
+                divContenedorDescripcion.classList.add("contenedor-actividad-caracteristicas");
+                let tituloDescripcion = document.createElement("h5");
+                tituloDescripcion.textContent = "Descripción"
+                let descripcion = document.createElement("p");
+                descripcion.textContent = actividad.descripcion;
+                divContenedorDescripcion.appendChild(tituloDescripcion);
+                divContenedorDescripcion.appendChild(descripcion);
+
+                let divContenedorCompetencia = document.createElement("div");
+                divContenedorCompetencia.classList.add("contenedor-actividad-caracteristicas");
+                let tituloCompetencia = document.createElement("h5");
+                tituloCompetencia.textContent = "Competencia"
+                let competencia = document.createElement("p");
+                competencia.textContent = actividad.competencia;
+                divContenedorCompetencia.appendChild(tituloCompetencia);
+                divContenedorCompetencia.appendChild(competencia);
+
+                let divContenedorBeneficios = document.createElement("div");
+                divContenedorBeneficios.classList.add("contenedor-actividad-caracteristicas");
+                let tituloBeneficios = document.createElement("h5");
+                tituloBeneficios.textContent = "Beneficios"
+                let beneficios = document.createElement("p");
+                beneficios.textContent = actividad.beneficios;
+                divContenedorBeneficios.appendChild(tituloBeneficios);
+                divContenedorBeneficios.appendChild(beneficios);
+
+                let divContenedorCreditos = document.createElement("div");
+                divContenedorCreditos.classList.add("contenedor-actividad-caracteristicas");
+                let tituloCreditos = document.createElement("h5");
+                tituloCreditos.textContent = "Creditos que otorga"
+                let creditos = document.createElement("p");
+                creditos.textContent = actividad.creditos_otorga;
+                divContenedorCreditos.appendChild(tituloCreditos);
+                divContenedorCreditos.appendChild(creditos);
+
+                let divBotonGrupos = document.createElement("div");
+                divBotonGrupos.classList.add("contenedor-actividad-boton");
+                let boton = document.createElement("button");
+                boton.textContent = "Mostrar grupos";
+                boton.classList.add("boton-mostrar-grupos");
+                boton.onclick = function() {
+                    let contenedorGrupos = document.getElementById("actividad" + actividad.id_actividad);
+                    if (contenedorGrupos.style.display === "none") {
+                        contenedorGrupos.style.display = "block";
+                        boton.textContent = "Ocultar grupos"
+                    } else {
+                        contenedorGrupos.style.display = "none";
+                        boton.textContent = "Mostrar grupos";
+                    }
+                }
+                divBotonGrupos.appendChild(boton);
+
+                contenedorActividad.appendChild(divContenedorDescripcion);
+                contenedorActividad.appendChild(divContenedorCompetencia);
+                contenedorActividad.appendChild(divContenedorBeneficios);
+                contenedorActividad.appendChild(divContenedorCreditos);
+                contenedorActividad.appendChild(divBotonGrupos);
+
                 let divContenido = document.createElement("div")
                 divContenido.classList.add("contenedor-grupos");
                 divContenido.id = "actividad" + actividad.id_actividad;
-                divContenido.setAttribute("hidden", true);
+                divContenido.style.display = "none";
                 programa.appendChild(divTitulo);
+                programa.appendChild(contenedorActividad);
                 programa.appendChild(divContenido);
             });
         }
@@ -243,7 +370,7 @@
                 divTitulo.classList.add("contenedor-grupo-titulo");
                 divTitulo.setAttribute("onclick", "mostrarContenido()");
                 let h3 = document.createElement("h3");
-                h3.textContent = "Grupo "+grupo.nombre;
+                h3.textContent = "Grupo " + grupo.nombre;
                 divTitulo.appendChild(h3);
                 let divContenido = document.createElement("div")
                 divContenido.classList.add("descripcion-grupo");
@@ -254,8 +381,6 @@
             });
         }
         crearGrupos();
-
-
     </script>
 </body>
 
